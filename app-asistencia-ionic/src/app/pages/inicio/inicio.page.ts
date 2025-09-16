d4449167bd6ce516629d2d246cd565c84ee4624a
Benjatapia20 <be.tapiar@duocuc.cl>
2025-09-16 13:47:48 -0300

    Update inicio.page.ts

--- app-asistencia-ionic/src/app/pages/inicio/inicio.page.ts
+++ app-asistencia-ionic/src/app/pages/inicio/inicio.page.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
     selector: 'app-inicio',
     templateUrl: './inicio.page.html',
     styleUrls: ['./inicio.pages.scss'],
-    standalone: false
+    standalone: false,
 })
 //InicioPage es la página principal después de la página Login.
 export class InicioPage implements OnInit, OnDestroy {
