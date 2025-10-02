e7fdb16be655283b08b24d5300dc92e6ef5f84fc
Benjatapia20 <be.tapiar@duocuc.cl>
2025-10-02 11:59:55 -0300

    Update not-found-routing.module.ts

--- app-asistencia-ionic/src/app/pages/not-found/not-found-routing.module.ts
+++ app-asistencia-ionic/src/app/pages/not-found/not-found-routing.module.ts
@@ -1,7 +1,10 @@
+//Define las ruta específica para la página NotFound
+//Indica a Angular qué componentes, directivas o servicios se usan en la página.
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { NotFoundPage } from "./not-found.page";
 
+//Este archivo conecta la URL con la página que debe mostrarse cuando la ruta no existe.
 const routes: Routes = [
     {
         path: '',
