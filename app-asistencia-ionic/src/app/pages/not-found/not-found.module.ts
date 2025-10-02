398f1f7ddc4394b6e3d6ecdbc6e527eef1e7724d
Benjatapia20 <be.tapiar@duocuc.cl>
2025-10-02 12:02:10 -0300

    Update not-found.module.ts

--- app-asistencia-ionic/src/app/pages/not-found/not-found.module.ts
+++ app-asistencia-ionic/src/app/pages/not-found/not-found.module.ts
@@ -1,8 +1,11 @@
+//Módulo de Angular que agrupa todo lo relacionado con la página.
+//Le dice a Angular los componentes, directivas o servicios que se usan en la página.
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 
+//Se importan NotFoundPage con su routing.module para poder usarlo en las rutas.
 import { NotFoundPageRoutingModule } from "./not-found-routing.module";
 import { NotFoundPage } from "./not-found.page";
 
